99bd34911ad6c80661a1c929a2c7cfc8deeb4850
KingOfTheGamesYT <teamolympuscaptain@gmail.com>
2020-06-03 22:11:23 -0400

    Updated code to 1.14.4
    Got rid of unused classes

--- src/main/java/com/modmcdl/endgameextension/EndgameExtension.java
+++ src/main/java/com/modmcdl/endgameextension/EndgameExtension.java
@@ -2,12 +2,10 @@ package com.modmcdl.endgameextension;
 
 
 import com.modmcdl.endgameextension.init.ModItems;
-import com.modmcdl.endgameextension.proxies.CommonProxy;
-import com.modmcdl.endgameextension.util.handlers.RegistryHandler;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
+
 
 
 @Mod.EventBusSubscriber(modid = EndgameExtension.MODID, bus = Mod.EventBusSubscriber.Bus.FORGE)
@@ -18,10 +16,7 @@ public class EndgameExtension
 	public static final String NAME = "Endgame Extension";
 	public static final String VERSION = "2.0.0";
 	public static final String ACCEPTEDVERSIONS = "[1.14.4]";
-	
-	public static final String CLIENT_PROXY_CLASS = "com.modmcdl.endgameextension.proxies.ClientProxy";
-	public static final String COMMON_PROXY_CLASS = "com.modmcdl.endgameextension.proxies.CommonProxy";
-	
+
 
 	public static final ItemGroup ITEM_GROUP = (new ItemGroup("EndgameExtension") {
 		@Override
