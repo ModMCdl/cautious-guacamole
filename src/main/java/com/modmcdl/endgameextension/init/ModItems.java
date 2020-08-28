e8c9ed3c568b04cc48e808f30b9bde0fa343e19b
KingOfTheGamesYT <teamolympuscaptain@gmail.com>
2020-08-28 04:37:38 -0400

    Fixed Mod Items
    Made Mod items in the 1.14.4 format

--- src/main/java/com/modmcdl/endgameextension/init/ModItems.java
+++ src/main/java/com/modmcdl/endgameextension/init/ModItems.java
@@ -1,9 +1,5 @@
 package com.modmcdl.endgameextension.init;
 
-import com.modmcdl.endgameextension.items.ItemQuenchedStar;
-import com.modmcdl.endgameextension.items.ItemStarShard;
-import com.modmcdl.endgameextension.items.tools.*;
-import com.modmcdl.endgameextension.util.handlers.MaterialHandler;
 import net.minecraft.item.Item;
 
 import java.util.ArrayList;
@@ -14,15 +10,15 @@ public class ModItems
 	public static final List<Item> ITEMS = new ArrayList<Item>();
 	
 	//-----Item List-----//
-	public static final Item QUENCHED_STAR = new ItemQuenchedStar("quenched_nether_star");
-	public static final Item NETHER_STAR_SHARD = new ItemStarShard("nether_star_shard");
+	public static  Item QUENCHED_STAR;
+	public static  Item NETHER_STAR_SHARD;
 	
 	//-----Tools-----//
 	
-	public static  Item WITHER_PICKAXE = new WitherPickaxe("wither_pickaxe", MaterialHandler.WITHER);
-	public static  Item WITHER_SWORD = new WitherSword("wither_sword", MaterialHandler.WITHER);
-	public static  Item WITHER_AXE = new WitherAxe("wither_axe", MaterialHandler.WITHER);
-	public static  Item WITHER_HOE = new WitherHoe("wither_hoe", MaterialHandler.WITHER);
-	public static  Item WITHER_SPADE = new WitherShovel("wither_shovel", MaterialHandler.WITHER);
+	public static  Item WITHER_PICKAXE;
+	public static  Item WITHER_SWORD;
+	public static  Item WITHER_AXE;
+	public static  Item WITHER_HOE;
+	public static  Item WITHER_SPADE;
 	
 }
