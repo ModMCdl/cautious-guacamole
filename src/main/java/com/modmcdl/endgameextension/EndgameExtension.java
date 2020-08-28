31c928106008940dcf4f696c8df8d654ae529ce3
KingOfTheGamesYT <teamolympuscaptain@gmail.com>
2020-08-28 03:00:20 -0400

    Updated Main Class
    moved the Item Groups and tools and blocks over to the correct spot for 1.14.4

--- src/main/java/com/modmcdl/endgameextension/EndgameExtension.java
+++ src/main/java/com/modmcdl/endgameextension/EndgameExtension.java
@@ -2,26 +2,65 @@ package com.modmcdl.endgameextension;
 
 
 import com.modmcdl.endgameextension.init.ModItems;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 
-
-@Mod.EventBusSubscriber(modid = EndgameExtension.MODID, bus = Mod.EventBusSubscriber.Bus.FORGE)
-public class EndgameExtension
-{
+@Mod("endgameextension")
+public class EndgameExtension {
 	//References
-	public static final String MODID = "endgameextension";
+	public static EndgameExtension instance;
+	public static final String modid = "endgameextension";
 	public static final String NAME = "Endgame Extension";
 	public static final String VERSION = "2.0.0";
 	public static final String ACCEPTEDVERSIONS = "[1.14.4]";
+	public static final Logger Logger = LogManager.getLogger(modid);
 
+	public static final ItemGroup ITEM_GROUP = new EndgameExtension() {
 
-	public static final ItemGroup ITEM_GROUP = new ItemGroup("EndgameExtension") {
-		@Override
-		public ItemStack createIcon() {
-			return new ItemStack(ModItems.QUENCHED_STAR);
+		public EndgameExtension() {
+			instance = this;
+			FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+			FMLJavaModLoadingContext.get().getModEventBus().addListener(this::clientRegistries);
+			MinecraftForge.EVENT_BUS.register(this);
 		}
+
+		private void setup(final FMLCommonSetupEvent event) {
+			Logger.info("Setup method registered.");
+		}
+
+		private void clientRegistries(final FMLClientSetupEvent event) {
+			Logger.info("clientRegistries method registered.");
+		}
+
 	};
+
+	@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
+	public static class RegistryEvents {
+		@SubscribeEvent
+		public static void registerItems(final RegistryEvent.Register<Item> event) {
+			event.getRegistry().registerAll
+					(
+							ModItems.WITHER_AXE = new ItemAxe
+					);
+			Logger.info("Items registered.");
+		}
+
+		private static ResourceLocation location(String name) {
+			return new ResourceLocation(modid, name);
+		}
+
+	}
 }
