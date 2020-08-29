f8990ff790c47c8bd5bbec4a32288583ccc3de97
KingOfTheGamesYT <teamolympuscaptain@gmail.com>
2020-08-29 02:45:15 -0400

    removed
    These were moved to the main file in 1.14.4

--- src/main/java/com/modmcdl/endgameextension/util/handlers/MaterialHandler.java
+++
@@ -1,8 +0,0 @@
-package com.modmcdl.endgameextension.util.handlers;
-
-
-public class MaterialHandler 
-{
-	public static final ToolMaterial WITHER = EnumHelper.addToolMaterial("wither", 10, 3124, 50, 8F, 18);
-	//public static final ToolMaterial TESTER = EnumHelper.addToolMaterial(name, harvestLevel, maxUses, efficiency, damage, enchantability)
-}
