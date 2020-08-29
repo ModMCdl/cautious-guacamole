22658c5e235e53f8946b4aac07f9b0a15e2ee832
KingOfTheGamesYT <teamolympuscaptain@gmail.com>
2020-08-29 02:49:08 -0400

    Updated Block List
    Updated mod block list for 1.14.4

--- src/main/java/com/modmcdl/endgameextension/init/ModBlocks.java
+++ src/main/java/com/modmcdl/endgameextension/init/ModBlocks.java
@@ -1,10 +1,6 @@
 package com.modmcdl.endgameextension.init;
 
-import com.modmcdl.endgameextension.blocks.BlockNetherStar;
-import com.modmcdl.endgameextension.blocks.BlockSoulBricks;
-import com.modmcdl.endgameextension.blocks.FlowerAsphodel;
 import net.minecraft.block.Block;
-import net.minecraft.block.material.Material;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,10 +12,10 @@ public class ModBlocks
 	
 	//-----Block List-----//
 	
-	public static final Block ASPHODEL = new FlowerAsphodel("flower_asphodel", Block.Properties.create(Material.PLANTS));
-	public static final Block NETHER_STAR_BLOCK = new BlockNetherStar("block_of_nether_star", Material.GLASS);
+	public static  Block FLOWER_ASPHODEL;
+	public static  Block NETHER_STAR_BLOCK;
 	
-	public static final Block SOUL_BRICKS = new BlockSoulBricks("soul_bricks", Material.ROCK);
-	public static final Block CHISELED_SOUL_BRUCKS = new BlockSoulBricks("chiseled_soul_bricks", Material.ROCK);
+	public static  Block SOUL_BRICKS;
+	public static  Block CHISELED_SOUL_BRICKS;
 	
 }
