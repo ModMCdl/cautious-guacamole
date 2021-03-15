aa43a20e16b3419ae356f0e4c515eee2a0fb76cd
KingOfTheGamesYT <teamolympuscaptain@gmail.com>
2021-03-15 01:02:08 -0400

    Ported over all wither tools
    and sword to 1.14.4

--- src/main/java/com/modmcdl/endgameextension/RegistryEvents.java
+++ src/main/java/com/modmcdl/endgameextension/RegistryEvents.java
@@ -1,6 +1,5 @@
 package com.modmcdl.endgameextension;
 
-import com.modmcdl.endgameextension.init.EndgameExpansion;
 import com.modmcdl.endgameextension.init.ModBlocks;
 import com.modmcdl.endgameextension.init.ModItems;
 import com.modmcdl.endgameextension.init.ToolMaterialList;
@@ -12,59 +11,62 @@ import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+
+import java.util.logging.Logger;
+
+import static com.modmcdl.endgameextension.EndgameExtension.ENDGAME;
 
 
-
-@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
-public class RegistryEvents {
-
-    public static final ItemGroup endgame = EndgameExpansion.endgame;
-    public static EndgameExtension instance;
-    public static final String modid = "endgameextension";
-    public static final String NAME = "Endgame Extension";
-    public static final String VERSION = "2.0.0";
-    public static final String ACCEPTEDVERSIONS = "[1.14.4]";
-    public static final Logger Logger = LogManager.getLogger(modid);
+@Mod.EventBusSubscriber(bus=Mod.EventBusSubscriber.Bus.MOD)
+public class RegistryEvents
+{
+    public static final ItemGroup EndGame = ENDGAME;
+    public static final Logger LOGGER = EndgameExtension.LOGGER;
+    public static final String MODID = EndgameExtension.MODID;
 
     @SubscribeEvent
-    public static void registerItems(final RegistryEvent.Register<Item> event) {
+    public static void registerItems(final RegistryEvent.Register<Item> event)
+    {
         event.getRegistry().registerAll
                 (
-                        ModItems.QUENCHED_STAR = new Item(new Item.Properties().group(endgame)).setRegistryName(location("quenched_star")));
-        ModItems.NETHER_STAR_SHARD = new Item(new Item.Properties().group(endgame)).setRegistryName(location("nether_star_shard"));
+                        ModItems.quenched_star = new Item(new Item.Properties().group(EndGame)).setRegistryName(location("quenched_nether_star")),
 
-        ModItems.WITHER_AXE = new AxeItem(ToolMaterialList.nether_star_shard, -1.0f, 1.0f, new Item.Properties().group(endgame)).setRegistryName(location("wither_axe"));
-        ModItems.WITHER_HOE = new HoeItem(ToolMaterialList.nether_star_shard, 9.0f, new Item.Properties().group(endgame)).setRegistryName(location("wither_hoe"));
-        ModItems.WITHER_PICKAXE = new PickaxeItem(ToolMaterialList.nether_star_shard, -2, 1.2f, new Item.Properties().group(endgame)).setRegistryName(location("wither_pickaxe"));
-        ModItems.WITHER_SPADE = new ShovelItem(ToolMaterialList.nether_star_shard, -1.5f, 1.0f, new Item.Properties().group(endgame)).setRegistryName(location("wither_shovel"));
-        ModItems.WITHER_SWORD = new SwordItem(ToolMaterialList.nether_star_shard, 0, 1.6f, new Item.Properties().group(endgame)).setRegistryName(location("wither_sword"));
-
-        ModItems.FLOWER_ASPHODEL = new BlockItem(ModBlocks.FLOWER_ASPHODEL, new Item.Properties().group(endgame)).setRegistryName(ModBlocks.FLOWER_ASPHODEL.getRegistryName());
-        ModItems.CHISELED_SOUL_BRICKS = new BlockItem(ModBlocks.CHISELED_SOUL_BRICKS, new Item.Properties().group(endgame)).setRegistryName(ModBlocks.CHISELED_SOUL_BRICKS.getRegistryName());
-        ModItems.NETHER_STAR_BLOCK = new BlockItem(ModBlocks.NETHER_STAR_BLOCK, new Item.Properties().group(endgame)).setRegistryName(ModBlocks.NETHER_STAR_BLOCK.getRegistryName());
-        ModItems.SOUL_BRICKS = new BlockItem(ModBlocks.SOUL_BRICKS, new Item.Properties().group(endgame)).setRegistryName(ModBlocks.SOUL_BRICKS.getRegistryName());
+                       ModItems.wither_axe = new AxeItem(ToolMaterialList.nether_star_shard, -1.0f, 1.0f, new Item.Properties().group(EndGame)).setRegistryName(location("wither_axe")),
+                       ModItems.wither_hoe = new HoeItem(ToolMaterialList.nether_star_shard, 9.0f, new Item.Properties().group(EndGame)).setRegistryName(location("wither_hoe")),
+                       ModItems.wither_pickaxe = new PickaxeItem(ToolMaterialList.nether_star_shard, -2, 1.2f, new Item.Properties().group(EndGame)).setRegistryName(location("wither_pickaxe")),
+                       ModItems.wither_spade = new ShovelItem(ToolMaterialList.nether_star_shard, -1.5f, 1.0f, new Item.Properties().group(EndGame)).setRegistryName(location("wither_shovel")),
+                     ModItems.wither_sword = new SwordItem(ToolMaterialList.nether_star_shard, 0, 1.6f, new Item.Properties().group(EndGame)).setRegistryName(location("wither_sword")),
 
 
+                        ModItems.nether_star_block = new BlockItem(ModBlocks.nether_star_block, new Item.Properties().group(EndGame)).setRegistryName(ModBlocks.nether_star_block.getRegistryName()),
+                       ModItems.soul_bricks = new BlockItem(ModBlocks.soul_bricks, new Item.Properties().group(EndGame)).setRegistryName(ModBlocks.soul_bricks.getRegistryName()),
+                       ModItems.chiseled_soul_bricks = new BlockItem(ModBlocks.chiseled_soul_bricks, new Item.Properties().group(EndGame)).setRegistryName(ModBlocks.chiseled_soul_bricks.getRegistryName()),
+                       ModItems.flower_asphodel = new BlockItem(ModBlocks.flower_asphodel, new Item.Properties().group(EndGame)).setRegistryName(ModBlocks.flower_asphodel.getRegistryName())
 
 
-        Logger.info("Items registered.");
+
+
+        );
+   }
+
+    @SubscribeEvent
+    public static void registerBlocks(final RegistryEvent.Register<Block> event)
+   {
+       event.getRegistry().registerAll
+          (
+                  ModBlocks.nether_star_block = new Block(Block.Properties.create(Material.GLASS).hardnessAndResistance(50.0F, 6442450944F).lightValue(2).sound(SoundType.GLASS)).setRegistryName(location("nether_star_block")),
+                       ModBlocks.soul_bricks = new Block(Block.Properties.create(Material.ROCK).hardnessAndResistance(30F,6000F).sound(SoundType.GROUND)).setRegistryName(location("soul_bricks")),
+                     ModBlocks.chiseled_soul_bricks = new Block(Block.Properties.create(Material.ROCK).hardnessAndResistance(0.0F).sound(SoundType.GROUND)).setRegistryName(location("chiseled_soul_bricks")),
+                        ModBlocks.flower_asphodel = new Block(Block.Properties.create(Material.PLANTS).hardnessAndResistance(0.0f).sound(SoundType.PLANT)).setRegistryName(location("flower_asphodel"))
+                );
+
+
+   }
+
+
+
+    public static ResourceLocation location(String name)
+    {
+        return new ResourceLocation(MODID, name);
     }
-    public static void registerBlocks(final RegistryEvent.Register<Block> event) {
-        event.getRegistry().registerAll
-                (
-
-                        ModBlocks.FLOWER_ASPHODEL = new Block(Block.Properties.create(Material.PLANTS).hardnessAndResistance(0.0F).sound(SoundType.PLANT)).setRegistryName(location("flower_asphodel")));
-        ModBlocks.CHISELED_SOUL_BRICKS = new Block(Block.Properties.create(Material.ROCK).hardnessAndResistance(0.0F).sound(SoundType.STONE)).setRegistryName(location("chiseled_soul_bricks"));
-        ModBlocks.NETHER_STAR_BLOCK = new Block(Block.Properties.create(Material.GLASS).hardnessAndResistance(50.0F, 6442450944F).sound(SoundType.GLASS).lightValue(2)).setRegistryName(location("nether_star_block"));
-        ModBlocks.SOUL_BRICKS = new Block(Block.Properties.create(Material.ROCK).hardnessAndResistance(30F,6000F).sound(SoundType.STONE).harvestLevel(10)).setRegistryName(location("soul_bricks"));
-
-        ;
-        Logger.info("Blocks registered.");
-    }
-    private static ResourceLocation location(String name) {
-        return new ResourceLocation(modid, name);
-    }
-
 }
