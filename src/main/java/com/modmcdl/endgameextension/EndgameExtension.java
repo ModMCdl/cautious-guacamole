16dfb54cab23d2c0a44fe0f576d87f949d4d862e
KingOfTheGamesYT <teamolympuscaptain@gmail.com>
2021-03-15 00:28:02 -0400

    moved the creative tab to the main class
    for better orginization

--- src/main/java/com/modmcdl/endgameextension/EndgameExtension.java
+++ src/main/java/com/modmcdl/endgameextension/EndgameExtension.java
@@ -1,44 +1,49 @@
 package com.modmcdl.endgameextension;
 
 
-import com.modmcdl.endgameextension.init.EndgameExpansion;
+import com.modmcdl.endgameextension.init.ModItems;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import java.util.logging.LogManager;
+
+import java.util.logging.Logger;
 
 
 @Mod("endgameextension")
 public class EndgameExtension {
 	//References
 	public static EndgameExtension instance;
-	public static final String modid = "endgameextension";
-	public static final String NAME = "Endgame Extension";
-	public static final String VERSION = "2.0.0";
-	public static final String ACCEPTEDVERSIONS = "[1.14.4]";
-	public static final Logger Logger = LogManager.getLogger(modid);
-	public static final ItemGroup ENDGAME = EndgameExpansion.endgame;
+	public static final String MODID = "endgameextension";
+	public static final Logger LOGGER = LogManager.getLogManager().getLogger(MODID);
 
 
-		public EndgameExtension()
-		{
-			instance = this;
-			FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
-			FMLJavaModLoadingContext.get().getModEventBus().addListener(this::clientRegistries);
-			MinecraftForge.EVENT_BUS.register(this);
-		}
+	public EndgameExtension() {
+		instance = this;
+		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::clientRegistries);
+		MinecraftForge.EVENT_BUS.register(this);
+	}
 
-		private void setup(final FMLCommonSetupEvent event) {
-			Logger.info("Setup method registered.");
-		}
+	private void setup(final FMLCommonSetupEvent event) {
+		Logger.getLogger("Setup method registered.");
+	}
 
-		private void clientRegistries(final FMLClientSetupEvent event) {
-			Logger.info("clientRegistries method registered.");
+	private void clientRegistries(final FMLClientSetupEvent event) {
+		Logger.getLogger("clientRegistries method registered.");
+	}
+
+
+	public static final ItemGroup ENDGAME = new ItemGroup("endgame") {
+		@Override
+		public ItemStack createIcon() {
+			return new ItemStack(ModItems.quenched_star);
 		}
 
 	};
+}
 
